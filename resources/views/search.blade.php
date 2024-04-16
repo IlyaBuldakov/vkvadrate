cb4bbc52bce7ff7e93e51b33ab45b04654877d63
Myddll <denis.ahatov@nutnet.ru>
2024-04-16 16:56:34 +0400

    Рефакторинг: представление отредактировано в соответствии с рефакторингом

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,4 +1,3 @@
-@php use App\Http\Controllers\SearchController;use App\Models\service\MarketSignService; @endphp
 @extends('layouts/app')
 
 @section('title', 'Поиск')
@@ -19,33 +18,37 @@
             <br>
 
             <div class="row pb-5 mb-4">
-                @foreach($items as $item)
-                    <div class="col-lg-4 col-md-6" style="margin-bottom: 20px">
-                        <div class="card rounded shadow-sm border-0 mb-5 h-100">
-                            <div class="card-body p-4"><img
-                                    src="{{ $item->photoUrl }}" alt=""
-                                    class="img-fluid d-block mx-auto mb-3">
-                                <h5><a href="{{ $item->itemUrl }}" class="text-dark">{{ $item->name }}</a></h5>
-                                <p class="small text-muted font-italic">
-                                    Текст для описания товара
-                                </p>
-                                <div class="market__box float-start">
-                                    <div class="row">
-                                            <img src="{{ asset(MarketSignService::getLogoByName($market)) }}"
+                @foreach($marketplacesResults as $marketplacesResult)
+                    @foreach($marketplacesResult->getItems() as $item)
+                        <div class="col-lg-4 col-md-6" style="margin-bottom: 20px">
+                            <div class="card rounded shadow-sm border-0 mb-5 h-100">
+                                <div class="card-body p-4"><img
+                                        src="{{ $item->photoUrl }}" alt=""
+                                        class="img-fluid d-block mx-auto mb-3">
+                                    <h5><a href="{{ $item->itemUrl }}"
+                                           class="text-dark">{{ $item->name }}</a></h5>
+                                    <p class="small text-muted font-italic">
+                                        Текст для описания товара
+                                    </p>
+                                    <div class="market__box float-start">
+                                        <div class="row">
+                                            <img src="{{ $marketplacesResult->getLogoPath() }}"
                                                  style="width: 70px !important; height: 45px !important;" alt="">
-                                        <div class="col">
-                                            <p class="small text-muted font-italic" style="margin-top: 0.65rem !important">
-                                                SimaLand
-                                            </p>
+                                            <div class="col">
+                                                <p class="small text-muted font-italic"
+                                                   style="margin-top: 0.65rem !important">
+                                                    SimaLand
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="price__box float-end">
-                                    <h4>{{ $item->price }} ₽</h4>
+                                    <div class="price__box float-end">
+                                        <h4>{{ $item->price }} ₽</h4>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 @endforeach
             </div>
         </div>
