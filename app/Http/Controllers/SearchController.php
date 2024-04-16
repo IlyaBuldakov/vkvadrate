ee32d5e5b011bfa43815c5ba41d9cc7939be61da
Myddll <denis.ahatov@nutnet.ru>
2024-04-16 16:56:46 +0400

    Рефакторинг: контроллер отредактирован в соответствии с рефакторингом

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\service\CommonSearchService;
+
+/**
+ * Контроллер поиска товаров по текстовому запросу.
+ */
 class SearchController extends Controller
 {
+    /**
+     * Получить список товаров со всех маркетплейсов по текстовому запросу.
+     */
     public function getByQuery()
     {
         $query = urlencode($_GET['query']);
-        $curl = curl_init("https://www.sima-land.ru/api/v3/item/?page_type=search&q=$query");
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'x-api-key: 8de4c439f49586e3ac53a134cb0e15dd61825b1c0dba9d55b86087b28d4e4b799c6e1ecfc213f16b47b723f7e71157d71bdf909f1959c7e01b02d296f548ef20'));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $jsonObject = json_decode(curl_exec($curl));
+        $commonSearch = new CommonSearchService();
+        $marketplacesResults = $commonSearch->search($query);
+
         return view('search',
             [
-                'market' => 'sima',
-                'items' => $jsonObject->items,
+                'marketplacesResults' => $marketplacesResults,
             ]);
     }
 }
