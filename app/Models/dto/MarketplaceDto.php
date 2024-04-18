b11f4727b8784f36fe5e3174e0bfb28f4edc5c9f
Илья <ggpanguin@gmail.com>
2024-04-18 11:03:26 +0400

    Отредактировано DTO маркетплейса

--- app/Models/dto/MarketplaceDto.php
+++ app/Models/dto/MarketplaceDto.php
@@ -8,10 +8,13 @@ class MarketplaceDto
 
     private array $items;
 
-    public function __construct(string $logoPath, array $items,)
+    private string $sign;
+
+    public function __construct(string $logoPath, array $items, string $sign)
     {
         $this->logoPath = $logoPath;
         $this->items = $items;
+        $this->sign = $sign;
     }
 
     /**
@@ -28,5 +31,10 @@ class MarketplaceDto
     public function getItems(): array
     {
         return $this->items;
+    }
+
+    public function getSign(): string
+    {
+        return $this->sign;
     }
 }
