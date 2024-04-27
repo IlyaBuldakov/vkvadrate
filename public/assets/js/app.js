bc201bd8941c4ebafe8fa980ef72250601d576d1
Myddll <denis.ahatov@nutnet.ru>
2024-04-27 10:23:57 +0400

    Отредактированы статические файлы

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,4 @@
+// ------ Preloader ------
 function preLoader() {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
@@ -6,7 +7,64 @@ function preLoader() {
     }, 500);
 }
 
-
 if (document.readyState !== "complete") {
     preLoader();
+}
+// ------ Preloader ------
+
+var marketplaceButtonsBox = document.querySelector('.marketplace__button_box');
+
+window.onload = function() {
+    document.querySelectorAll('.marketplace__button').forEach(
+        function (button) {
+            if (!(params.get('filters') === 'all')) {
+                var filters = params.get('filters');
+                if (!filters.split(',').includes(button.innerHTML)) {
+                    toggleFilterButton(button);
+                }
+            }
+        }
+    );
+}
+
+function marketplaceFilterButtonAction(element) {
+    toggleFilterButton(element);
+
+    if (document.getElementsByClassName('filter_submit__button').length < 1) {
+        marketplaceButtonsBox.innerHTML +=
+            "<button class=\"btn marketplace__button filter_submit__button\" onclick=\"searchWithFilters()\" type=\"submit\"><i class=\"fa fa-check\"></i></button>";
+    }
+}
+
+function searchWithFilters() {
+    var queryString = "?filters=";
+    var resultArray = [];
+    var marketplaceFilterButtons = document.getElementsByClassName('marketplace__button');
+
+    Array.from(
+        marketplaceFilterButtons,
+        function (button) {
+            if (button.hasAttribute('enabled')) {
+                resultArray.push(button.innerHTML);
+            }
+        }
+    );
+
+    queryString += resultArray;
+
+    if (params.has('query')) {
+        queryString += '&query=' + params.get('query');
+    }
+    params.set('filters', '');
+    window.location.replace('/search' + queryString);
+}
+
+function toggleFilterButton(button) {
+    button.toggleAttribute('enabled');
+
+    if (button.hasAttribute('enabled')) {
+        button.style="background-color: #f9900e; color: white";
+    } else {
+        button.style="background-color: white; color: black";
+    }
 }
--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,11 +1,75 @@
-window.onload = function() {
-    var searchInput = document.querySelector('#search__input');
+var searchInput = document.querySelector('#search__input');
 
-    let params = new URL(document.location.toString()).searchParams;
+let params = new URL(document.location.toString()).searchParams;
+
+sendWsMessageAndDrawHtml();
+
+function sendWsMessageAndDrawHtml() {
     let query = params.get("query");
 
     if (query !== null) {
         searchInput.value = query;
-        // TODO: WebSocket
+        var ws = new WebSocket('ws://127.0.0.1:2345');
+
+        ws.onmessage = function (evt) {
+            var basket = document.querySelector('#basket');
+
+            var marketplaceDto = JSON.parse(evt.data);
+
+            var marketplaceNameBox = document.querySelector('#marketplace_name');
+            marketplaceNameBox.innerHTML = marketplaceDto.sign;
+
+            var marketplaceItems = marketplaceDto.items;
+
+            marketplaceItems.forEach(function (item) {
+                basket.innerHTML
+                    += " <div class=\"col-lg-4 col-md-6\" style=\"margin-bottom: 20px\">\n" +
+                    "                            <div class=\"card rounded shadow-sm border-0 mb-5 h-100\">\n" +
+                    "                                <div class=\"card-body p-4\"><img\n" +
+                    "                                        src=\"" + item.photoUrl + "\" alt=\"\"\n" +
+                    "                                        class=\"img-fluid d-block mx-auto mb-3\">\n" +
+                    "                                    <h5><a href=\"" + item.itemUrl + "\"\n" +
+                    "                                           class=\"text-dark\">" + item.name + "</a></h5>\n" +
+                    "                                    <p class=\"small text-muted font-italic\">\n" +
+                    "                                        Текст для описания товара\n" +
+                    "                                    </p>\n" +
+                    "                                    <div class=\"market__box float-start\">\n" +
+                    "                                        <div class=\"row\">\n" +
+                    "                                            <img src=\"" + marketplaceDto.logoPath + "\"\n" +
+                    "                                                 style=\"width: 75px !important; height: 45px !important;\" alt=\"\">\n" +
+                    "                                            <div class=\"col\">\n" +
+                    "                                                <p class=\"small text-muted font-italic\"\n" +
+                    "                                                   style=\"margin-top: 0.65rem !important\">\n" +
+                    "                                                    " + marketplaceDto.sign + "\n" +
+                    "                                                </p>\n" +
+                    "                                            </div>\n" +
+                    "                                        </div>\n" +
+                    "                                    </div>\n" +
+                    "                                    <div class=\"price__box float-end\">\n" +
+                    "                                        <h4>" + item.price + " ₽</h4>\n" +
+                    "                                    </div>\n" +
+                    "                                </div>\n" +
+                    "                            </div>\n" +
+                    "                        </div>";
+            });
+        };
+
+        waitForSocketConnection(ws, function () {
+            ws.send([params.get('filters'), query])
+        });
+    }
+
+    function waitForSocketConnection(socket, callback) {
+        setTimeout(
+            function () {
+                if (socket.readyState === 1) {
+                    if (callback != null) {
+                        callback();
+                    }
+                } else {
+                    waitForSocketConnection(socket, callback);
+                }
+
+            }, 5);
     }
 }
