7898a4eb8ab82055c57044734d477062e4708ebd
Myddll <denis.ahatov@nutnet.ru>
2024-04-27 10:24:48 +0400

    Добавлен WebSocket сервер

---
+++ app/Models/service/websocket.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\dto\MarketplaceDto;
+use App\Models\service\market\SimaLandService;
+use App\Models\service\market\WildberriesService;
+use App\Models\service\market\YandexMarketService;
+use Workerman\Worker;
+
+require_once '../../../vendor/autoload.php';
+
+$marketplaceServices =
+    [
+        new WildberriesService(),
+        new YandexMarketService(),
+    ];
+
+$ws_worker = new Worker('websocket://127.0.0.1:2345');
+
+$ws_worker->onConnect = function ($connection) {
+    echo "Соединение открыто\n";
+};
+
+$ws_worker->onMessage = function ($connection, $data) use ($marketplaceServices) {
+    $preparedData = explode(',', $data);
+    foreach ($marketplaceServices as $marketplaceService) {
+        if (in_array($marketplaceService->getSign(), explode(',', $preparedData[0])) ||
+            $preparedData[0] === 'all') {
+            var_dump(1);
+            $connection->send(
+                (new MarketplaceDto(
+                    $marketplaceService->getLogoPath(), $marketplaceService->search($preparedData[1]), $marketplaceService->getSign()
+                ))->__toString()
+            );
+        }
+    }
+};
+
+$ws_worker->onClose = function ($connection) {
+    echo "Соединение закрыто\n";
+};
+
+Worker::runAll();
